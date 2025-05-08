709d6b0bb35792b669255bbe16100d08ebcba561
Agustin Velazquez <agubolso2@gmail.com>
2025-05-08 03:43:15 -0300

    fix deploy

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: process.env.CLIENT_URL }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 
 // Log de rutas
