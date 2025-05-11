95c251d4c8d89f39c9f5dee9d1987db71a3f62f4
Agustin Velazquez <agubolso2@gmail.com>
2025-05-11 07:17:15 -0300

    fix id problem

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -6,8 +6,8 @@ const verifyToken = require("../middleware/verifyToken");
 // Rutas para reseñas existentes
 router.post("/", reviewController.createReview);
 router.get("/:bookId", reviewController.getReviewsByBook);
-router.get("/", reviewController.getAllReviews);
 
+// router.get("/", reviewController.getAllReviews);
 // Las siguientes rutas están comentadas porque aún no están implementadas
 // router.get("/user", verifyToken, reviewController.getReviewsByUser);
 // router.get("/:id", reviewController.getReviewById);
