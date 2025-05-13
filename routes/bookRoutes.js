97fc44f63c76f268b4f59fd381a9aa211916bfd6
Agustin Velazquez <agubolso2@gmail.com>
2025-05-13 11:16:33 -0300

    first day, fix routes auth in /id , ver detalles

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,23 +1,19 @@
 // routes/bookRoutes.js
-
 const express = require("express");
 const router = express.Router();
 const bookController = require("../controllers/bookController");
 const verifyToken = require("../middleware/verifyToken"); // Middleware de autenticación
 
-// Rutas CRUD protegidas con JWT
+// Rutas públicas (sin autenticación)
+router.get("/", bookController.getAllBooks); // Obtener todos los libros
+router.get("/:id", bookController.getBookById); // Obtener un libro por ID
+
+// Rutas protegidas con JWT (requieren autenticación)
 router.post("/", verifyToken, bookController.createBook); // Crear un libro
-router.get("/", verifyToken, bookController.getAllBooks); // Obtener todos los libros
-router.get("/:id", verifyToken, bookController.getBookById); // Obtener un libro por ID
 router.put("/:id", verifyToken, bookController.updateBook); // Actualizar un libro
 router.delete("/:id", verifyToken, bookController.deleteBook); // Eliminar un libro
 
-// Nueva ruta para cargar libros desde JSON
-// Con autenticación
-
-// router.post("/load", verifyToken, bookController.loadBooksFromJson);
-
-// O sin autenticación (solo comentar la línea de arriba y descomentar esta)
+// Ruta para cargar libros desde JSON
 router.post("/load", bookController.loadBooksFromJson);
 
 module.exports = router;
